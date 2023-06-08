d54923c899a2ae186588ee4e8ce615218df5288b
pkosciak <pkosciak@webpacket.io>
2023-06-08 22:11:28 +0200

    Init

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog</title>
+</head>
+<body>
+    <div class="container">
+        post content
+        <a href="/">go back</a>
+    </div>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog</title>
+</head>
+<body>
+    <div class="container">
+        <a href="/post">post</a>
+        <a href="/post">post</a>
+        <a href="/post">post</a>
+    </div>
+</body>
+</html>
